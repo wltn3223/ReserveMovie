b482811c53a33f4b9a404634945ba1a5548a8128
Mun Jisu <answltn8@gmail.com>
2023-10-22 14:26:26 +0900

    상영관VO 수정

--- src/model/CinemaVO.java
+++ src/model/CinemaVO.java
@@ -3,18 +3,27 @@ package model;
 import java.util.Objects;
 
 public class CinemaVO {
+    private int cinemaID;
+
     private int cinemaNo;
     private String moiveTitle;
     private int totalSeats;
     private int remainSeats;
     private String startTime;
+    private String finishTime;
 
-    public CinemaVO(int cinemaNo, String moiveTitle, int totalSeats, int remainSeats, String startTime) {
+    public CinemaVO(int cinemaID, int cinemaNo, String moiveTitle, int totalSeats, int remainSeats, String startTime, String finishTime) {
+        this.cinemaID = cinemaID;
         this.cinemaNo = cinemaNo;
         this.moiveTitle = moiveTitle;
         this.totalSeats = totalSeats;
         this.remainSeats = remainSeats;
         this.startTime = startTime;
+        this.finishTime = finishTime;
+    }
+
+    public int getCinemaID() {
+        return cinemaID;
     }
 
     public int getCinemaNo() {
@@ -37,6 +46,10 @@ public class CinemaVO {
         return startTime;
     }
 
+    public String getFinishTime() {
+        return finishTime;
+    }
+
     public void setRemainSeats(int remainSeats) {
         this.remainSeats = this.totalSeats - remainSeats;
     }
@@ -47,11 +60,11 @@ public class CinemaVO {
             return false;
         }
         CinemaVO cinema = (CinemaVO) obj;
-        return cinemaNo == cinema.cinemaNo;
+        return cinemaID == cinema.cinemaID;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(cinemaNo);
+        return Objects.hash(cinemaID);
     }
 }
