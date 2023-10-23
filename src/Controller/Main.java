106a04c4566ba74c58079859eef7c31e9b8a9a47
Mun Jisu <answltn8@gmail.com>
2023-10-24 06:12:41 +0900

    view 화면 추가

---
+++ src/Controller/Main.java
@@ -0,0 +1,170 @@
+package Controller;
+
+import model.*;
+
+import service.*;
+import view.Mainview;
+
+import java.util.ArrayList;
+
+
+public class Main {
+    public static void main(String[] args) {
+        MemberloginService memberloginService = new MemberloginService();
+        MemberTicketService memberTicketService = new MemberTicketService();
+        AdminCinemaService adminCinemaService = new AdminCinemaService();
+        AdminPlayingMovieService adminPlayingMovieService = new AdminPlayingMovieService();
+        AdminMovieService adminMovieService = new AdminMovieService();
+        MemberVO memberVO = null;
+        boolean flag = false;
+        boolean adminmode = false;
+        while (!flag) {
+            try {
+                String menuChoice = Mainview.getMenuChoice();
+                switch (menuChoice) {
+
+                    case "0":
+                        System.out.println("시스템을 종료합니다.");
+                        return;
+                    case "1":
+                        memberVO = memberloginService.login();
+                        if (memberVO != null) {
+                            flag = true;
+                        }
+                        break;
+                    case "2":
+                        memberloginService.join();
+                        break;
+                    case "3":
+                        memberloginService.updatepassword();
+                        break;
+                    case "4":
+                        memberloginService.resignation();
+                        break;
+                    case "admin1234":
+                        System.out.println("관리자로그인");
+                        flag = true;
+                        adminmode = true;
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println("잘못입력하셨습니다. 다시 입력해주세요.");
+            }
+        }
+
+        if (adminmode) {
+            while (true) {
+                try {
+                    String adminMenu = Mainview.getAdminMenu();
+                    switch (adminMenu) {
+                        case "0":
+                            System.out.println("시스템을 종료합니다.");
+                            return;
+                        case "1":
+                            boolean stopFlag = false;
+                            while (!stopFlag){
+                            String movieAdminMenu = Mainview.getMovieAdminMenu();
+                                switch (movieAdminMenu){
+                                    case "0":
+                                        stopFlag = true;
+                                    break;
+                                    case "1":
+                                        adminMovieService.insertMovie();
+                                    break;
+                                    case "2":
+                                        adminMovieService.printMovie();
+                                        break;
+                                    case "3":
+                                        adminMovieService.updateMovie();
+                                        break;
+                                    case "4":
+                                        adminMovieService.deleteMovie();
+                                        break;
+                                }
+                            }
+                            break;
+                        case "2":
+                            boolean stopFlag2 = false;
+                            while (!stopFlag2){
+                            String cinemaAdminMenu = Mainview.getCinemaAdminMenu();
+                                switch (cinemaAdminMenu){
+                                    case "0":
+                                        stopFlag2 = true;
+                                        break;
+                                    case "1":
+                                        adminCinemaService.addCinema();
+                                        break;
+                                    case "2":
+                                        adminCinemaService.printCinemaList();
+                                        break;
+                                    case "3":
+                                        adminCinemaService.updateCinemaSeats();
+                                        break;
+                                    case "4":
+                                        adminCinemaService.deleteCinema();
+                                        break;
+                                }
+                            }
+                            break;
+                        case "3":
+                            boolean stopFlag3 = false;
+                            while (!stopFlag3){
+                            String playingMovieAdminMenu = Mainview.getPlayingMovieAdminMenu();
+                                switch (playingMovieAdminMenu){
+                                    case "0":
+                                        stopFlag3 = true;
+                                        break;
+                                    case "1":
+                                        adminPlayingMovieService.addPlayingMovie();
+                                        break;
+                                    case "2":
+                                        adminPlayingMovieService.printPlayingMovieList();
+                                        break;
+                                    case "3":
+                                        adminPlayingMovieService.updatePlayingMovie();
+                                        break;
+                                    case "4":
+                                        adminPlayingMovieService.deletePlayingMovie();
+                                        break;
+                                }
+                            }
+                            break;
+                    }
+
+                } catch (Exception e) {
+                    System.out.println("잘못입력하셨습니다. 다시 입력해주세요.");
+                }
+
+            }
+
+        }
+        flag = false;
+        while (!flag){
+            try {
+                String userReserveMenu = Mainview.getUserReserveMenu();
+                switch (userReserveMenu){
+                    case "0":
+                        flag = true;
+                    case "1":
+                        memberTicketService.reserveMovie(memberVO.getiD());
+                        break;
+                    case "2":
+                        memberTicketService.viewTicketList(memberVO.getiD());
+                        break;
+                    case "3":
+                        memberTicketService.cancle(memberVO.getiD());
+                        break;
+                    case "4":
+                        memberTicketService.changeSeat(memberVO.getiD());
+
+                }
+
+            }catch (Exception e){
+                System.out.println("잘못 입력하셨습니다. 다시 입력해주세요");
+            }
+        }
+        System.out.println("시스템을 종료합니다.");
+
+
+    }
+}
--- src/view/Mainview.java
+++ src/view/Mainview.java
@@ -5,23 +5,52 @@ import java.io.InputStreamReader;
 
 public class Mainview {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static String getMenuChoice() throws  Exception{
-        StringBuffer sb = new StringBuffer("안녕하세요 미래 영화관입니다.");
+
+    public static String getMenuChoice() throws Exception {
+        StringBuffer sb = new StringBuffer();
+        sb.append("안녕하세요 미래 영화관입니다.");
         sb.append("\n무엇을 도와드릴까요.? 1. 로그인 2. 회원가입 3.비밀번호 변경 4. 회원 탈퇴 0. 종료");
         System.out.println(sb);
         return br.readLine();
 
     }
-    public static String getUserMenu() throws  Exception{
+
+    public static String getUserMenu() throws Exception {
         System.out.println("1. 영화 예매 2. 영화 예매 내역 확인3. 영화 예매 취소 4. 좌석 변경 0. 종료");
         return br.readLine();
     }
 
-    public static void adminMenu(){
-        System.out.println("관리자 메뉴");
-        System.out.println("관리자 메뉴 상영 영화 추가, 영화 수정, 영화 제거, 영화 조회");
-        System.out.println("관리자 메뉴 ");
+    public static String getAdminMenu() throws Exception {
+        StringBuffer sb = new StringBuffer();
+        sb.append("관리자 메뉴");
+        sb.append("\n1. 영화 추가, 조회, 수정, 제거");
+        sb.append("\n2. 상영관 추가, 조회, 좌석 수정, 상영관 제거");
+        sb.append("\n3. 상영 영화 추가, 조회, 수정, 제거");
+        sb.append("\n0.종료.");
+        System.out.println(sb);
+        return br.readLine();
     }
+
+    public static String getMovieAdminMenu() throws Exception {
+        System.out.println("1. 영화 추가 2. 영화 목록 조회 3. 영화 수정 4. 영화 삭제 0. 돌아가기");
+        return br.readLine();
+    }
+
+    public static String getCinemaAdminMenu() throws Exception {
+        System.out.println("1. 상영관 추가 2. 상영관 조회 3. 상영관 수정 4. 상영관 제거 0. 돌아가기");
+        return br.readLine();
+    }
+
+    public static String getPlayingMovieAdminMenu() throws Exception {
+        System.out.println("1. 상영 영화 추가 2. 상영 영화 조회 3. 상영 영화 수정 4. 상영할 영화 제거 0. 돌아가기");
+        return br.readLine();
+    }
+
+    public static String getUserReserveMenu() throws Exception {
+        System.out.println("1. 영화 예매 2. 영화 예매 내역 확인 3. 영화 예매 취소 4. 좌석 변경 0. 종료");
+        return br.readLine();
+    }
+
 
 
 
